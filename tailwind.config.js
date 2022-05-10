1cc6e7d2b0ac143984ccdc12ced5bcb05cfdff18
HabiburRahmanAnik <habib264521@gmail.com>
2022-05-10 15:06:26 +0600

    add another component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'banner-chair': "url('/src/assets/images/bg.png')",
+      }
+    },
   },
 
   daisyui: {
