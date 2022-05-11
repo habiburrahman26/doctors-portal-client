fe033228f2a420184be442d2a4bed776a0322aa9
HabiburRahmanAnik <habib264521@gmail.com>
2022-05-11 20:09:45 +0600

    complete services part on appoinment page

--- src/components/pages/Appoinment/Services.js
+++ src/components/pages/Appoinment/Services.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import Button from '../Shared/Button';
 
 const Services = (props) => {
   console.log(props);
   return (
-    <div className="shadow-md p-4 rounded-md text-center">
-      <h3 className="text-2xl text-secondary font-semibold mb-1">
+    <div className="shadow-md py-8 rounded-lg text-center">
+      <h3 className="text-xl text-secondary font-semibold mb-1">
         {props.name}
       </h3>
       <p>{props.slots[0]}</p>
-      <p>10 space avaible</p>
+      <p className='mb-4'>10 space avaible</p>
+      <Button text="book appoinment" />
     </div>
   );
 };
